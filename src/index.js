8c1d93e9be2360c39606cd739d884b998d710ce4
angelina <floresangela.0707@gmail.com>
2021-08-08 18:28:31 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let exprArr = [];
+    let exprDotDash = [];
+    let result = [];
+  
+    let tmpNumChar = 0;
+    for(let i = 0; i < expr.length; i+=10){
+      tmpNumChar = expr.substr(i, 10).indexOf('1');
+      exprArr.push(expr.substr(i, 10).substr(tmpNumChar));
+    }
+  
+    for(word of exprArr){
+      exprDotDash.push(word.replaceAll('10', '.').replaceAll('11', '-').replaceAll('*', ' '));
+    }
+  
+    for(word of exprDotDash){
+      if(word !== ' '){
+        result.push(MORSE_TABLE[word]);
+      }else{
+        result.push(word);
+      }
+    }
+  
+    result = result.join('');
+    
+    return result;
+  }
 
 module.exports = {
     decode
